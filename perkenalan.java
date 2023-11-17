c9d5c0497549da8cdaf749ddd514748f6a679b3c
RejaArdan <151033874+RejaArdan@users.noreply.github.com>
2023-11-17 09:08:56 +0700

    Add files via upload

---
+++ perkenalan.java
@@ -0,0 +1,23 @@
+package identitas;
+
+    public class perkenalan {
+        public static void main(String[] arg ){
+            //membuat variabel
+            String ngaran, alamat;
+            int usia;
+            double tinggi;
+            
+            //mengisi variabel
+            ngaran="Ayi Reja Ardani";
+            alamat="Afganistan palih kaler";
+            usia= 19;
+            tinggi= 161.5;
+            //memcetak ke layar isi variabel 
+            System.out.println("Assalamualaikum Bro Bro Perkenalkan");
+            System.out.println("Nama Saya    :" + ngaran);
+            System.out.println("Alamat       :Saya Tinggal di" + alamat);
+            System.out.println("Usia Saya    :" + usia);
+            System.out.println("Tinggi Badan :" + tinggi + "cm");
+
+        }
+    }
